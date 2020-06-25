ad5c4f1968e540cf775d631b63059677320fed31
Pablo Gonzalez <pabgon0@gmail.com>
2020-06-25 22:57:31 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,13 +1,5 @@
 class Author < ActiveRecord::Base
-<<<<<<< HEAD
-    validates :name, presence: true
-    validates :email, uniqueness: true
-    validates :phone_number, length: {is: 10}
-  end
-  
-=======
   validates :name, presence: true
   validates :email, uniqueness: true
   validates :phone_number, length: {is: 10}
 end
->>>>>>> 4d92d509e60576a7d5c6aada5773d4a182f8e022
